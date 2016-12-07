ec369202cd27e1c84f36d3e2851df1a059bb1da2
Ehsan Mokhtari <ehsanm94@gmail.com>
2016-12-07 12:58:34 +0330

    change result root

--- app/Response.php
+++ app/Response.php
@@ -8,6 +8,7 @@ class Response
 	private $error;
 
 	const ROOT = 'response';
+	const RESULT = 'result';
 
 	public function __construct($result = null, $ok = true, $error = null) {
 		$this->ok 		= $ok;
@@ -19,7 +20,7 @@ class Response
 	private function createJsonResponse() {
 		$response = array_merge(
 			array('ok' => $this->ok),
-			$this->error ? $this->error->toArray() : array(self::ROOT => $this->result)
+			$this->error ? $this->error->toArray() : array(self::RESULT => $this->result)
 		);
 		return json_encode(array(self::ROOT => $response));
 	}
@@ -27,7 +28,7 @@ class Response
 	private function createXMLResponse() {
 		$response = array_merge(
 			array('ok' => $this->ok),
-			$this->error ? $this->error->toArray() : array(self::ROOT => $this->result)
+			$this->error ? $this->error->toArray() : array(self::RESULT => $this->result)
 		);
 		$response = Array2XML::createXML(self::ROOT, $response);
 		return $response->saveXML();
